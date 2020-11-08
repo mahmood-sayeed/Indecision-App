54604afa190aa91f1130811bc68e116f3cda9d45
mahmood-sayeed <mahmood.sayeed8@gmail.com>
2020-11-08 11:01:48 +0530

    setting things up

---
+++ src/app.js
@@ -0,0 +1,16 @@
+console.log('app.js running');
+
+//JSX - Javascript XML
+var template = (
+	<div>
+		<h1>Indecision App</h1>
+		<p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+	</div>
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
