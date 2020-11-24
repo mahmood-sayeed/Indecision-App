ff7f4fbef96cfc69d788dae1a81a40e741dc12ad
mahmood-sayeed <mahmood.sayeed8@gmail.com>
2020-11-24 14:09:14 +0530

    Avoid Global Modules, Make them local.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,8 +8,8 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-//live-server public
-//babel src\app.js --out-file=public\scripts\app.js --presets=env,react --watch
+//yarn run serve
+//yarn run build
 //two different terminals on path react\indecision-app
 
 var IndecisionApp = function (_React$Component) {
--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-//live-server public
-//babel src\app.js --out-file=public\scripts\app.js --presets=env,react --watch
+//yarn run serve
+//yarn run build
 //two different terminals on path react\indecision-app
 
 class IndecisionApp extends React.Component {
