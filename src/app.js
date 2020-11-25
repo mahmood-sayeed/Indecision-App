6a31b96df51d54810ed9ff99db4d4d616f1f5393
mahmood-sayeed <mahmood.sayeed8@gmail.com>
2020-11-25 12:59:46 +0530

    ES6 import/export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('running');
+// import './utils.js';
+// import { square, add } from './utils.js';
+
+// console.log('running');
+// console.log(square(4));
+// console.log(add(100, 23));
+
+import { isAdult, canDrink } from './person.js';
+console.log('can drink ', canDrink(19));
+console.log('Adult?  ', isAdult(20));
---
+++ src/utils.js
@@ -0,0 +1,9 @@
+console.log('utils.js');
+
+export const square = (x) => x * x;  //named
+
+export const add = (a, b) => a + b;
+
+//export { square, add }; //named
+
+//exports - single default or many named exports
