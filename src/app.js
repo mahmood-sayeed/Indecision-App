585e1c0e23995f90cea87535cba2af7dcdc00d3a
mahmood-sayeed <mahmood.sayeed8@gmail.com>
2020-11-25 15:48:36 +0530

    Deafult Exports

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 // import './utils.js';
-// import { square, add } from './utils.js';
-
+// import subtract, { square, add } from './utils.js';
+// //default import can be named anything. subtract can be called "anything"
 // console.log('running');
 // console.log(square(4));
 // console.log(add(100, 23));
+// console.log(subtract(100, 81));
 
-import { isAdult, canDrink } from './person.js';
+import isSenior, { isAdult, canDrink } from './person.js';
 console.log('can drink ', canDrink(19));
 console.log('Adult?  ', isAdult(20));
+console.log(isSenior(64));
--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,12 @@ export const square = (x) => x * x;  //named
 
 export const add = (a, b) => a + b;
 
-//export { square, add }; //named
+const subtract = (a, b) => a - b;
+
+export default subtract;
+
+//export default (a, b) => a - b;
+
+// export { square, add, subtract as default }; //named 2, subtract default
 
 //exports - single default or many named exports
