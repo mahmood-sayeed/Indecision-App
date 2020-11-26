bcc3dfd282ce6d547d7b0ce4ce50cce87732250a
mahmood-sayeed <mahmood.sayeed8@gmail.com>
2020-11-26 18:49:12 +0530

    Webpack DevServer ContentBase

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 //yarn run build
 //yarn run serve
+//yarn run dev-server
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,10 @@ module.exports = {
             exclude: /node_modules/
         }]
     },
-    devtool: 'cheap-module-eval-source-map'
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
 };
 
 //babel-loader extension which teaches webpack how to run babel
